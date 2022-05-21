59bac077d583f80c22aac5965ed9bea78096138a
長尾 拓輝 <21010017nh@edu.fca.ac.jp>
2022-05-21 19:48:48 +0900

    キー入力によってスフィアオブジェクトを操作するスクリプトを追加 コンポーネントを読み込めるようにもした

---
+++ Assets/scripts/learn521.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class learn521 : MonoBehaviour
+{
+    [SerializeField]
+    private int count;
+
+    [SerializeField]
+    private Rigidbody rigidBody;
+
+    private void Awake()
+    {
+        //rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スペースを押したらバウンド止まる
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+}
